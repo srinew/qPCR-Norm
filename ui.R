730bd8b1fa7fc220c5a3f9e50637d65b9f63345e
Srinivas Nallandhighal <33036242+srinew@users.noreply.github.com>
2020-02-05 15:53:08 -0500

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,45 @@
+ui <- fluidPage(
+  
+  HTML('<meta name="viewport" content="width=1024">'),
+  # Application title
+  titlePanel(div(HTML("CT value Normalized Heatmap for Morgan lab"))),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Choose CSV File",
+                accept = c(
+                  "text/csv",
+                  "text/comma-separated-values,text/plain",
+                  ".csv")
+      ),
+      # checkboxInput("header", "Header", TRUE), 
+      radioButtons("select_class_1",
+                   "Sample Clustering:",
+                   selected = c("Yes"),
+                   choiceNames = list("Yes",
+                                      "No"),
+                   choiceValues = list("Yes",
+                                       "No"),
+                   width = 200),
+      radioButtons("select_class_2",
+                   "Clustering method:",
+                   selected = c("ward.D2"),
+                   choiceNames = list("ward.D2",
+                                      "ward.D",
+                                      "single",
+                                      "complete",
+                                      "average",
+                                      "median"),
+                   choiceValues = list("ward.D2",
+                                       "ward.D",
+                                       "single",
+                                       "complete",
+                                       "average",
+                                       "median"),
+                   width = 200),
+      downloadButton("export", label = "download heatmap"),
+      width = 2),
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+)
